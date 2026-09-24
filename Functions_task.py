97f4cd5d1466cb1edd740ecf1d1cc0f9f0444799
ShravanRaj101 <shravanchintu7@gmail.com>
2026-09-24 11:42:23 +0530

    Add files via upload

---
+++ Functions_task.py
@@ -0,0 +1,131 @@
+#1
+def welcome():
+    print("Welcome to Python")
+welcome()
+
+#2
+def greet():
+    print("Hello Students")
+greet()
+
+#3
+def display_name():
+    print("Shravan")
+display_name()
+
+#4
+def show_course():
+    print("Python Full Stack")
+show_course()
+
+#5
+def institute():
+    print("10000coders")
+institute()
+
+#6
+def greet(name):
+    print("Hello",name)
+greet("Siva")
+
+#7
+def square(n):
+    print(n**2)
+square(5)
+
+#8
+def display_age(age):
+    print(age)
+display_age(25)
+
+#9
+def student(name):
+    print("Student Name:",name)
+student("Shravan")
+
+#10
+def double(n):
+    print(n+n)
+double(10)
+
+#11
+def add(a,b):
+    print("Sum of two numbers is:",a+b)
+add(10,30)
+
+#12
+def sub(a,b):
+    print("Substraction of twoo nums is:",a-b)
+sub(100,10)
+
+#13
+def mul(a,b):
+    print("Product ofd two nuns is:",a*b)
+mul(19,40)
+
+#14
+def div(a,b):
+    print("Division of two nums is:",a/b)
+div(20,5)
+
+#15
+def student(name,age):
+    print("Name of the Student is",name,"and he is",age)
+student("Divya",21)
+
+#16
+def employee(name,salary):
+    print("Name of employee is",name,"and his salary is",salary)
+employee("Manoj",10000)
+
+#17
+def rect(length,width):
+    print("Area of rectangle is",length*width)
+rect(20,30)
+
+#18
+def total_marks(m1,m2,m3):
+    print("Total marks are:",m1+m2+m3)
+total_marks(80,90,70)
+
+#19
+def bill(price,quantity):
+    print("Total Bill is",price*quantity)
+bill(4000,12)
+
+#20
+def course(student_name,course_name):
+    print("student name is",student_name,"and course is",course_name)
+course("Shravan","MCA")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
